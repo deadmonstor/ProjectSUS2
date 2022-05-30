413a77e9b126baa6489c2655e62fe96978e99b48
Josh Mobley <deadmonstor69@gmail.com>
2022-05-30 10:34:09 +0100

    Project

---
+++ Assets/Scripts/Suck/SuckManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuckManager : MonoBehaviour
+{
+    private bool pulling = false;
+
+    
+    void FixedUpdate()
+    {
+        
+    }
+}
