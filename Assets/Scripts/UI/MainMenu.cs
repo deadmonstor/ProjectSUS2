f78fc733b449b6cb130e4e83023c13d14672c0f0
Ben Nilsson <55544010+BenNilsson@users.noreply.github.com>
2022-05-31 14:25:40 +0100

    MainMenu v1

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private CanvasGroup controls;
+    [SerializeField] private GameObject backButton;
+    [SerializeField] private GameObject startButton;
+    
     public void OnStart()
     {
         SceneManager.LoadScene(1);
@@ -17,5 +22,21 @@ public class MainMenu : MonoBehaviour
         #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
         #endif
+    }
+
+    public void OnControls()
+    {
+        EventSystem.current.SetSelectedGameObject(backButton);
+        controls.interactable = true;
+        controls.alpha = 1f;
+        controls.blocksRaycasts = true;
+    }
+
+    public void ExitControls()
+    {
+        EventSystem.current.SetSelectedGameObject(startButton);
+        controls.interactable = false;
+        controls.alpha = 0f;
+        controls.blocksRaycasts = false;
     }
 }
